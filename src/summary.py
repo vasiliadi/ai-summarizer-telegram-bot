698dd6be277ca97b6d8a165328ac8acba3e9b7f7
vasiliadi <9432426+vasiliadi@users.noreply.github.com>
2024-10-17 14:05:30 -0500

    fixes

--- src/summary.py
+++ src/summary.py
@@ -57,9 +57,10 @@ def summarize(data: str, use_transcription: bool, use_yt_transcription: bool) ->
 
     try:
         return summarize_with_file(data)
-    except (exceptions.RetryError, TimeoutError, exceptions.DeadlineExceeded):
+    except (exceptions.RetryError, TimeoutError, exceptions.DeadlineExceeded) as e:
         if use_transcription:
             new_file = f"{generate_temprorary_name().split('.')[0]}.ogg"
             compress_audio(input_file=data, output_file=new_file)
             transcription = transcribe(new_file)
             return f"**Transcript used:** {summarize_with_transcript(transcription)}"
+        raise Exception from e
