5c0e52ef39a6c7f285b0e925dfc4b3c24bb3d62f
vasiliadi <9432426+vasiliadi@users.noreply.github.com>
2024-10-28 16:12:05 -0500

    ✨ feat(download.py): add retry mechanism for YouTube downloads to handle transient errors (#75)
    🔧 fix(download.py): remove unnecessary file existence check after download

--- src/download.py
+++ src/download.py
@@ -3,7 +3,9 @@ from pathlib import Path
 
 import requests
 from bs4 import BeautifulSoup
+from tenacity import retry, retry_if_exception_type, stop_after_attempt, wait_fixed
 from yt_dlp import YoutubeDL
+from yt_dlp.utils import DownloadError
 
 from config import NUMERIC_LOG_LEVEL, PROXY
 from utils import generate_temporary_name
@@ -15,6 +17,12 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+@retry(
+    wait=wait_fixed(10),
+    retry=retry_if_exception_type(DownloadError),
+    reraise=True,
+    stop=stop_after_attempt(2),
+)  # type: ignore[call-overload]
 def download_yt(url: str) -> str:
     temprorary_file_name = generate_temporary_name()
     ydl_opts = {
@@ -31,8 +39,6 @@ def download_yt(url: str) -> str:
     }
     with YoutubeDL(ydl_opts) as ydl:
         ydl.download(url)
-    if not Path(temprorary_file_name).is_file():
-        raise OSError("Problem with downloading the file")
     return temprorary_file_name
 
 
