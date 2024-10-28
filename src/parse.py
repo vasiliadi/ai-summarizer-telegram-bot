02b5de216dc2cf652559428dc1a1edf656deed31
vasiliadi <9432426+vasiliadi@users.noreply.github.com>
2024-10-28 14:06:26 -0500

    Update README and modify type annotations in main, parse, and utils files (#73)
    * 📝 docs(README.md): update setup instructions to include migration steps and clarify Docker usage
    🔧 fix(README.md): specify that only SOCKS proxies are supported for WEB_SCRAPE_PROXY
    
    * 🔧 refactor(utils.py): improve type hinting for Message and UsersOrm imports to enhance code readability and maintainability
    🗑️ chore(utils.py): remove unused trafilatura functions to clean up the codebase and reduce complexity
    
    * ✨ feat(parse.py): add a new script to parse webpages using trafilatura
    
    * 🔧 refactor(main.py): update message type hints to use string annotations for better compatibility with TYPE_CHECKING
    🧹 chore(main.py): remove unused import of Message from telebot.types to clean up the codebase

---
+++ src/parse.py
@@ -0,0 +1,11 @@
+import trafilatura
+
+from config import WEB_SCRAPE_PROXY
+
+
+def parse_webpage(url: str) -> str:
+    trafilatura.downloads.PROXY_URL = WEB_SCRAPE_PROXY
+    downloaded = trafilatura.fetch_url(url, no_ssl=True)
+    if downloaded is None:
+        raise ValueError("No content to proceed")
+    return trafilatura.extract(downloaded)
