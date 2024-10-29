9df1fccfeb111005de4dfcea7dcc53ebdb67633f
vasiliadi <9432426+vasiliadi@users.noreply.github.com>
2024-10-28 20:52:11 -0500

    ✨ feat(parse.py): add retry mechanism for webpage parsing to handle MaxRetryError exceptions and improve robustness

--- src/parse.py
+++ src/parse.py
@@ -1,8 +1,16 @@
 import trafilatura
+from tenacity import retry, retry_if_exception_type, stop_after_attempt, wait_random
+from urllib3.exceptions import MaxRetryError
 
 from config import WEB_SCRAPE_PROXY
 
 
+@retry(
+    wait=wait_random(min=10, max=20),
+    retry=retry_if_exception_type(MaxRetryError),
+    reraise=True,
+    stop=stop_after_attempt(3),
+)  # type: ignore[call-overload]
 def parse_webpage(url: str) -> str:
     trafilatura.downloads.PROXY_URL = WEB_SCRAPE_PROXY
     downloaded = trafilatura.fetch_url(url, no_ssl=True)
