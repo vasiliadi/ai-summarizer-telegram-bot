01f7d79a4ab16be99ccbecbfa52e4cd9afc67b38
vasiliadi <9432426+vasiliadi@users.noreply.github.com>
2024-10-30 16:47:23 -0500

    🔧 chore(parse.py): refactor proxy configuration to use a dedicated proxies variable for better readability and maintainability

--- src/parse.py
+++ src/parse.py
@@ -8,12 +8,16 @@ def parse_webpage(url: str) -> str:
     headers = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",  # noqa: E501
     }  # https://www.whatismybrowser.com/guides/the-latest-user-agent/chrome
+    proxies = {
+        "http": WEB_SCRAPE_PROXY,
+        "https": WEB_SCRAPE_PROXY,
+    }
     downloaded = requests.get(
         requests.utils.requote_uri(url),
         verify=False,  # noqa: S501
         headers=headers,
         timeout=30,
-        proxies={"https": WEB_SCRAPE_PROXY},
+        proxies=proxies,
     )
     downloaded.raise_for_status()
     return trafilatura.extract(downloaded.text)
