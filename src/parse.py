13d3970dac51d1a9dc2f0d61d7f398f7390a4117
vasiliadi <9432426+vasiliadi@users.noreply.github.com>
2024-10-30 18:53:16 -0500

    🔄 refactor(parse.py): replace requests with Selenium for web scraping to handle dynamic content and improve reliability

--- src/parse.py
+++ src/parse.py
@@ -1,23 +1,21 @@
-import requests
 import trafilatura
+from requests.utils import requote_uri
+from selenium import webdriver
+from selenium.webdriver.common.proxy import Proxy, ProxyType
 
 from config import WEB_SCRAPE_PROXY
 
 
 def parse_webpage(url: str) -> str:
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",  # noqa: E501
-    }  # https://www.whatismybrowser.com/guides/the-latest-user-agent/chrome
-    proxies = {
-        "http": WEB_SCRAPE_PROXY,
-        "https": WEB_SCRAPE_PROXY,
-    }
-    downloaded = requests.get(
-        requests.utils.requote_uri(url),
-        verify=False,  # noqa: S501
-        headers=headers,
-        timeout=60,
-        proxies=proxies,
+    options = webdriver.ChromeOptions()
+    options.accept_insecure_certs = False
+    options.proxy = Proxy(
+        {"proxyType": ProxyType.MANUAL, "httpProxy": WEB_SCRAPE_PROXY},
     )
-    downloaded.raise_for_status()
-    return trafilatura.extract(downloaded.text)
+    options.add_argument("--headless=new")
+
+    with webdriver.Chrome(options=options) as driver:
+        driver.get(requote_uri(url))
+        html = driver.page_source
+
+    return trafilatura.extract(html)
