5bbeb66e49ad7a2e6b2167be4cb18b0214ed2b3a
vasiliadi <9432426+vasiliadi@users.noreply.github.com>
2024-10-30 20:34:25 -0500

    🔧 refactor(parse.py): remove retry logic and WebDriverException handling to simplify the parsing function ✨ feat(parse.py): streamline the webpage parsing process by eliminating unnecessary checks and improving readability

--- src/parse.py
+++ src/parse.py
@@ -3,19 +3,11 @@ import time
 import requests
 import trafilatura
 from selenium import webdriver
-from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.proxy import Proxy, ProxyType
-from tenacity import retry, retry_if_exception_type, stop_after_attempt, wait_fixed
 
 from config import WEB_SCRAPE_PROXY
 
 
-@retry(
-    wait=wait_fixed(10),
-    retry=retry_if_exception_type(WebDriverException),
-    reraise=True,
-    stop=stop_after_attempt(3),
-)  # type: ignore[call-overload]
 def parse_webpage(url: str) -> str:
     options = webdriver.ChromeOptions()
     options.accept_insecure_certs = False
@@ -29,7 +21,5 @@ def parse_webpage(url: str) -> str:
         driver.get(requests.utils.requote_uri(url))
         time.sleep(10)
         html = driver.page_source
-        if "Verifying you are human. This may take a few seconds." in html:  # WIP
-            raise WebDriverException("Could not pass bot verification")
 
     return trafilatura.extract(html)
