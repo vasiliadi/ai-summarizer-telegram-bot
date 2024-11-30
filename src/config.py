55130250e5f1383eb2fff23f0d6337f1c5346722
vasiliadi <9432426+vasiliadi@users.noreply.github.com>
2024-11-30 14:12:32 -0600

    🔧 fix(config.py): increase DAILY_LIMIT from 50 to 500 to allow higher usage rates

--- src/config.py
+++ src/config.py
@@ -92,7 +92,7 @@ headers = {
 MINUTE_LIMIT_KEY = "RPM"
 DAILY_LIMIT_KEY = "RPD"
 MINUTE_LIMIT = 2
-DAILY_LIMIT = 50
+DAILY_LIMIT = 500
 per_minute_limit = throttle.Throttle(
     limiter=periodic.PeriodicLimiter(
         store=redis_store.RedisStore(url=RATE_LIMITER_URL),
