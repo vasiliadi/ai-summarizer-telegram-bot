d8afcccdf8890ea3a843dead254223b5f054d3e8
vasiliadi <9432426+vasiliadi@users.noreply.github.com>
2025-11-12 11:57:37 -0600

    fix(config): update MODEL_ID_FOR_TRANSLATION to "gemini-2.5-flash" and enable thinking_config in translate function

--- src/config.py
+++ src/config.py
@@ -50,7 +50,7 @@ bot = telebot.TeleBot(token=TG_API_TOKEN, disable_web_page_preview=True)
 # Gemini config
 GEMINI_API_KEY = os.environ["GEMINI_API_KEY"]
 gemini_client = genai.Client(api_key=GEMINI_API_KEY)
-MODEL_ID_FOR_TRANSLATION = "gemini-2.0-flash"
+MODEL_ID_FOR_TRANSLATION = "gemini-2.5-flash"
 ALLOWED_MODELS_FOR_SUMMARY = [
     "gemini-2.5-flash",
     "gemini-2.5-pro",
